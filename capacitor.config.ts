5d68e7ef78889e4bde4cd8b78b8d5123f02984da
jebiyeon02 <jhje5595@naver.com>
2026-02-02 17:17:52 +0900

    build: 모바일 배포 환경 생성

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.onsikku.app',
+  appName: '온식구',
+  webDir: 'dist'
+};
+
+export default config;
